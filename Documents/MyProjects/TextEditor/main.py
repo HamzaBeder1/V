b04a3657143889256e1d80ec4d11debda64e8af7
HamzaBeder1 <hamzabeder0@gmail.com>
2023-08-06 03:03:33 -0700

    Second commit

--- Documents/MyProjects/TextEditor/main.py
+++ Documents/MyProjects/TextEditor/main.py
@@ -1,20 +1,43 @@
 from tkinter import *
+from tkinter import ttk
+
+a = Tk()
+files = ttk.Notebook(a)
+
+def new_file():
+    newFile = Frame(a)
+    files.add(newFile, text = 'Untitled')
+    horizontal_scroll = Scrollbar(newFile, width=30, orient=HORIZONTAL)
+    vertical_scroll = Scrollbar(newFile, width=30, orient=VERTICAL)
+    textbox = Text(newFile, xscrollcommand=horizontal_scroll.set, yscrollcommand=vertical_scroll.set, wrap="none")
+    vertical_scroll.config(command=textbox.yview)
+    horizontal_scroll.config(command=textbox.xview)
+
+    horizontal_scroll.pack(fill=X, side=BOTTOM)
+    vertical_scroll.pack(fill=Y, side=RIGHT)
+    textbox.pack(fill=BOTH, expand=True)
+    newFile.pack(fill=BOTH, expand=TRUE)
+    files.pack(side=TOP)
+
+def create_menu():
+    toolbar = Menu(a)
+
+    File = Menu(toolbar)
+    Edit = Menu(toolbar)
+    View = Menu(toolbar)
+    toolbar.add_cascade(label='File', menu=File)
+    toolbar.add_cascade(label='Edit', menu=Edit)
+    toolbar.add_cascade(label='View', menu=View)
+    File.add_command(label='New File', command=newFile)
+
+    a.config(menu=toolbar)
+
 
 def main():
-    a = Tk()
-    horizontalScroll = Scrollbar(a, width = 30, orient = HORIZONTAL)
-    verticalScroll = Scrollbar(a, width = 30, orient = VERTICAL)
-    horizontalScroll.pack(fill=X, side = BOTTOM)
-    verticalScroll.pack(fill=Y, side = RIGHT)
-    textbox = Text(a, xscrollcommand = horizontalScroll.set, yscrollcommand = verticalScroll.set, wrap = "none")
-    horizontalScroll.config(command=textbox.xview)
-    verticalScroll.config(command=textbox.yview)
-    textbox.pack(fill  = BOTH, expand = TRUE)
-
-    git
-
-
+    new_file()
+    create_menu()
     a.mainloop()
+
 
 if __name__ == "__main__":
     main()
