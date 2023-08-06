92d2c3b4b6a87c11637495ae90c556095185bd43
HamzaBeder1 <hamzabeder0@gmail.com>
2023-08-06 03:38:05 -0700

    Windows and file

--- Documents/MyProjects/TextEditor/main.py
+++ Documents/MyProjects/TextEditor/main.py
@@ -1,43 +1,48 @@
 from tkinter import *
 from tkinter import ttk
 
-a = Tk()
-files = ttk.Notebook(a)
-
+windows = []
+files = []
 def new_file():
-    newFile = Frame(a)
-    files.add(newFile, text = 'Untitled')
+    global windows
+    global files
+    newFile = Frame(files[len(files)-1])
+    files[len(files)-1].add(newFile, text = 'Untitled')
     horizontal_scroll = Scrollbar(newFile, width=30, orient=HORIZONTAL)
     vertical_scroll = Scrollbar(newFile, width=30, orient=VERTICAL)
     textbox = Text(newFile, xscrollcommand=horizontal_scroll.set, yscrollcommand=vertical_scroll.set, wrap="none")
     vertical_scroll.config(command=textbox.yview)
     horizontal_scroll.config(command=textbox.xview)
 
+    files[len(files)-1].pack(side=TOP, fill=BOTH)
     horizontal_scroll.pack(fill=X, side=BOTTOM)
     vertical_scroll.pack(fill=Y, side=RIGHT)
     textbox.pack(fill=BOTH, expand=True)
-    newFile.pack(fill=BOTH, expand=TRUE)
-    files.pack(side=TOP)
-
+def new_window():
+    tk = Tk()
+    file = ttk.Notebook(tk)
+    windows.append(tk)
+    files.append(file)
+    new_file()
+    create_menu()
 def create_menu():
-    toolbar = Menu(a)
-
+    global windows
+    toolbar = Menu(windows[len(windows)-1])
     File = Menu(toolbar)
     Edit = Menu(toolbar)
     View = Menu(toolbar)
     toolbar.add_cascade(label='File', menu=File)
     toolbar.add_cascade(label='Edit', menu=Edit)
     toolbar.add_cascade(label='View', menu=View)
-    File.add_command(label='New File', command=newFile)
+    File.add_command(label='New File', command=new_file)
+    File.add_command(label='New Window', command=new_window)
 
-    a.config(menu=toolbar)
+    windows[len(windows)-1].config(menu=toolbar)
 
 
 def main():
-    new_file()
-    create_menu()
-    a.mainloop()
-
+    new_window()
+    windows[len(windows)-1].mainloop()
 
 if __name__ == "__main__":
     main()
