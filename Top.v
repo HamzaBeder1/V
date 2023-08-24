5a2a05c1569cae7d29ed722c66e846ff0bffde67
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-24 14:25:51 -0700

    Update Top.v

--- Top.v
+++ Top.v
@@ -1,241 +1,133 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/01/2023 01:38:10 AM
-// Design Name: 
-// Module Name: Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module Top#(idle = 2'b00, sending_hour = 2'b01, sending_minutes = 2'b10, waiting = 2'b11)(
-    input clk, input [3:0] number, input [3:0] currLED, input transmit, output [3:0] anodeOutput, output [7:0] cathodeOutput, output reg Txd);
-    wire [3:0] LED0;
-    wire [3:0] LED1;
-    wire [3:0] LED2;
-    wire [3:0] LED3;
-    wire Done1;
-    wire Done2;
-    reg transmit1;
-    reg transmit2;
-    wire Txd1;
-    wire Txd2;
-    wire [15:0] hour;
-    wire [15:0] minutes;
-    reg [1:0] state = 0;
+module Top#(parameter debounce_count = 50000000, idle = 2'b00, sending = 2'b01, debouncing = 2'b10)(
+input clk, input rst, input transmit, input [3:0] currLED, input [3:0] number, output [3:0] anodeOutput, output [7:0] cathodeOutput, output Txd
+    );
+    wire [3:0] LED0, LED1, LED2, LED3;
+    wire [3:0] currNum;
+    wire [7:0] currASCII;
+    wire k;
+    reg d;
+    integer count;
+    integer debounce_counter;
+    reg UART_start;
+    wire UART_done;
+    assign currNum = (count == 0)? LED0: ((count == 1)? LED1: ((count == 2)? LED2: LED3));
+    assign k = (count == 4)? 1:0;
+    sevensegment s(clk, rst, number, currLED, anodeOutput, cathodeOutput, LED0, LED1, LED2, LED3);
+    UART_byte u(clk, rst, currASCII, UART_start, Txd, UART_done);
+    convertToASCII c(clk, currNum, currASCII);
     
-    reg [7:0] digit_0;
-    reg [7:0] digit_1;
-    reg [7:0] digit_2;
-    reg [7:0] digit_3;
-    reg [25:0] debounce_counter = 0;
+    reg [1:0] state, nextstate;
+    reg debounce;
     
-    
-    sevensegment a(clk, number, currLED, anodeOutput, cathodeOutput, LED0, LED1, LED2, LED3);
-    UARTTX b1(clk,hour[7:0], hour[15:8],transmit1,Txd1, Done1);
-    UARTTX b2(clk,minutes[7:0], minutes[15:8],transmit2,Txd2, Done2);
-    
-    always@(posedge clk)
+    always @(posedge rst)
     begin
-        case(LED0)
-            4'b0000:
-                digit_0 <= 8'b00110000;
-            4'b0001:
-                digit_0 <= 8'b00110001;
-            4'b0010:
-                digit_0 <= 8'b00110010;
-            4'b0011:
-                digit_0 <= 8'b00110011;
-            4'b0100:
-                digit_0 <= 8'b00110100;
-             4'b0101:
-                digit_0 <= 8'b00110101;
-             4'b0110:
-                digit_0 <= 8'b00110110;
-             4'b0111:
-                digit_0 <= 8'b00110111;
-             4'b1000:
-                digit_0 <= 8'b00110000;
-             4'b1001:
-                digit_0 <= 8'b00111001;
-             default:
-                digit_0 <= 8'b00000000;
-        endcase
-        
-        case(LED1)
-            4'b0000:
-                digit_1 <= 8'b00110000;
-            4'b0001:
-                digit_1 <= 8'b00110001;
-            4'b0010:
-                digit_1 <= 8'b00110010;
-            4'b0011:
-                digit_1 <= 8'b00110011;
-            4'b0100:
-                digit_1 <= 8'b00110100;
-             4'b0101:
-                digit_1 <= 8'b00110101;
-             4'b0110:
-                digit_1 <= 8'b00110110;
-             4'b0111:
-                digit_1 <= 8'b00110111;
-             4'b1000:
-                digit_1 <= 8'b00110000;
-             4'b1001:
-                digit_1 <= 8'b00111001;
-             default:
-                digit_1 <= 8'b00000000;
-        endcase
-        
-        case(LED2)
-            4'b0000:
-                digit_2 <= 8'b00110000;
-            4'b0001:
-                digit_2 <= 8'b00110001;
-            4'b0010:
-                digit_2 <= 8'b00110010;
-            4'b0011:
-                digit_2 <= 8'b00110011;
-            4'b0100:
-                digit_2 <= 8'b00110100;
-             4'b0101:
-                digit_2 <= 8'b00110101;
-             4'b0110:
-                digit_2 <= 8'b00110110;
-             4'b0111:
-                digit_2 <= 8'b00110111;
-             4'b1000:
-                digit_2 <= 8'b00110000;
-             4'b1001:
-                digit_2 <= 8'b00111001;
-             default:
-                digit_2 <= 8'b00000000;
-        endcase
-        
-        case(LED3)
-            4'b0000:
-                digit_3 <= 8'b00110000;
-            4'b0001:
-                digit_3 <= 8'b00110001;
-            4'b0010:
-                digit_3 <= 8'b00110010;
-            4'b0011:
-                digit_3 <= 8'b00110011;
-            4'b0100:
-                digit_3 <= 8'b00110100;
-             4'b0101:
-                digit_3 <= 8'b00110101;
-             4'b0110:
-                digit_3 <= 8'b00110110;
-             4'b0111:
-                digit_3 <= 8'b00110111;
-             4'b1000:
-                digit_3 <= 8'b00110000;
-             4'b1001:
-                digit_3 <= 8'b00111001;
-             default:
-                digit_3 <= 8'b00000000;
-        endcase
+        count <=0;
+        debounce_counter <=0;
     end
-    
-    always@(posedge clk)
+    always @(state, transmit, UART_done, k, d)
     begin
+        nextstate<=0;
+        UART_start<=0;
+        debounce<=0;
         case(state)
-            idle:
+        idle:
+        begin
+            if(~transmit)
             begin
-                if(transmit)
-                begin
-                    transmit1 <= 1;
-                    transmit2 <= 0;
-                    Txd <= Txd1;
-                    state <= sending_hour;
-                end
-                
-                else
-                begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= 1;
-                    state <= idle;
-                end
+                nextstate<=idle;
             end
-            
-            sending_hour:
+            else
             begin
-                if(Done1)
-                begin
-                    transmit1 <= 0;
-                    transmit2 <= 1;
-                    Txd <= Txd2;
-                    state <= sending_minutes;
-                end
-                
-                else
-                begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= Txd1;
-                    state <= sending_hour;
-                end
+                nextstate<=sending;
+                UART_start<=1;
             end
-            
-            sending_minutes:
+        end
+        
+        sending:
+        begin
+            if(~UART_done)
             begin
-                if(Done2)
-                begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= 1;
-                    state <= waiting;
-                end
-                
-                else
-                begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= Txd2;
-                    state <= sending_minutes;
-                end
+                nextstate<=sending;
+                UART_start <= 1;
             end
-            waiting:
+            else
             begin
-                if(debounce_counter == 50000000)
+                if(~k)
                 begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= 1;
-                    state <= idle;
-                    debounce_counter <= 0;
+                    UART_start<=1;
+                    nextstate<= sending;
                 end
                 else
                 begin
-                    transmit1 <= 0;
-                    transmit2 <= 0;
-                    Txd <= 1;
-                    state <= waiting;
-                    debounce_counter <= debounce_counter + 1;
+                    nextstate<= debouncing;
                 end
             end
+        end
+        
+        debouncing:
+        begin
+            if(~d)
+            begin
+                debounce<=1;
+                nextstate<=debouncing;
+            end
+            else
+            begin
+                nextstate<=idle;
+            end
+        end
         endcase
     end
     
-    
-    assign hour = {digit_1, digit_0};
-    assign minutes = {digit_3,  digit_2};
-    assign state_debug = state;
-    
-   
-    
+    always @(posedge clk)
+    begin
+        state<=nextstate;
+        if(debounce)
+        begin
+            if(debounce_counter == debounce_count)
+            begin
+                debounce_counter<=0;
+                d <= 1;
+            end
+            else
+            begin
+                debounce_counter <=debounce_counter+1;
+                d <= 0;
+            end
+        end
+        else
+            d<=0;
+    end
+endmodule
+
+module convertToASCII(input clk, input [3:0] currNum, output reg [7:0] currASCII);
+begin
+    always@(posedge clk)
+    begin
+        case(currNum)
+        4'b0000:
+            currASCII <= 8'b00110000;
+        4'b0001:
+            currASCII <= 8'b00110001;
+        4'b0010:
+            currASCII <= 8'b00110010;
+        4'b0011:
+            currASCII <= 8'b00110011;
+        4'b0100:
+            currASCII <= 8'b00110100;
+        4'b0101:
+            currASCII <= 8'b00110101;
+        4'b0110:
+            currASCII <= 8'b00110110;
+        4'b0111:
+            currASCII <= 8'b00110111;
+        4'b1000:
+            currASCII <= 8'b00111000;
+        4'b1001:
+            currASCII <= 8'b00111001;
+    endcase
+    end
+end
 endmodule
