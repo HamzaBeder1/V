abaa13d60a17e2b9635a10867c5da94e7cb7d46d
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-29 18:14:54 -0700

    general purpose cordic

--- coordic_algorithm/ex_logarithic_hyperbolic_functions/ex.v
+++ coordic_algorithm/ex_logarithic_hyperbolic_functions/ex.v
@@ -1,31 +1,14 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/26/2023 01:24:48 PM
-// Design Name: 
-// Module Name: ex
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module ex(input clk, input st, input compute, input [15:0] x, output [16:0] ex
+
+module ex(input clk, input st, input compute, input [15:0] x, input[3:0] func, output [31:0] result
 
     );
-    
+    wire [16:0] ex;
     wire [15:0] sinh;
     wire [15:0] cosh;
-    sinh_andcosh s(clk, st, compute, x, sinh, cosh);
+    sinh_andcosh s(clk, st, x, func,sinh, cosh, result);
     assign ex = sinh+cosh;
+    assign result = (func == 6)? ex: 32'bz;
 endmodule
