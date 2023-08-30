89ff92988fbd3f80c176dfac55fd59a639410e35
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-29 18:15:15 -0700

    general purpose cordic

--- coordic_algorithm/ex_logarithic_hyperbolic_functions/ln.v
+++ coordic_algorithm/ex_logarithic_hyperbolic_functions/ln.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ps
 
 module ln_x(
-    input clk, input st, input compute, input [15:0] x, output [16:0] ln
+    input clk, input st, input compute, input [15:0] x,input[3:0] func, output [31:0] ln
     );
-    
+    wire [16:0] ln;
     wire [15:0] x_1, y_1;
-    wire [15:0] result;
+    wire [31:0] result;
     assign x_1 = x+16'b0100000000000000;
     assign y_1 = x-16'b0100000000000000;
     
     arctan_h atanh (clk, st, compute,x_1, y_1, result);
     assign ln = result << 1;
+    assign result = (func == 8)? ln:32'bz;
     
 endmodule
