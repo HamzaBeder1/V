6e514371bd8b22dce5f820e1f00c089fb6a99d74
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 02:10:08 -0700

    Update ln.v

--- coordic_algorithm/ex_logarithic_hyperbolic_functions/ln.v
+++ coordic_algorithm/ex_logarithic_hyperbolic_functions/ln.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 
 module ln_x(
-    input clk, input st, input compute, input [15:0] x,input[3:0] func, output [31:0] ln
+    input clk, input st, input [15:0] x,input[3:0] func, output signed [31:0] result
     );
     wire [16:0] ln;
+    wire [15:0] ln_temp;
     wire [15:0] x_1, y_1;
-    wire [31:0] result;
     assign x_1 = x+16'b0100000000000000;
     assign y_1 = x-16'b0100000000000000;
     
-    arctan_h atanh (clk, st, compute,x_1, y_1, result);
-    assign ln = result << 1;
-    assign result = (func == 8)? ln:32'bz;
+    arctan_h atanh (clk, st,x_1, y_1, func, ln_temp, result);
+    assign ln = ln_temp <<< 1;
+    assign result = (func == 8)? ln:32'dz;
     
 endmodule
