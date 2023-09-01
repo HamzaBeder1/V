e994d5590c5e52b955305417ac30866115d879fc
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-09-01 13:11:27 -0700

    added comments

--- coordic_algorithm/coordic_arcsinandarccos/arcsin_andarccos.v
+++ coordic_algorithm/coordic_arcsinandarccos/arcsin_andarccos.v
@@ -1,24 +1,17 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/28/2023 02:14:24 AM
-// Design Name: 
-// Module Name: arcsin_andarccos
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+/*
+    This module is used for calculating the arcsin and arccos of the input.
+    
+    Inputs:
+            clk: Clock signal for controlling operations.
+            st: start signal that begins the CORDIC iterations
+            arcsin_in: Angle to compute the arcsin of.
+            func: A register that determines if the output from the module should write to the result register and which function to write.
+            
+   Outputs:
+            result: This is a bus shared by the modules in this project. Depending on func, this module either writes High-Z or the output it computes.
+*/
 
 module arcsin_andarccos#(parameter n = 16) (input clk, input st, input signed [15:0] arcsin_in, input [3:0] func, output signed [31:0] result
 
@@ -34,15 +27,10 @@ module arcsin_andarccos#(parameter n = 16) (input clk, input st, input signed [1
     
     wire signed [15:0] arcsin, arccos;
     wire done;
-    
-    assign result = (func == 2)? arccos :((func == 3)?arcsin:32'dz);
-    assign k = (i == n)? 1:0;
-    assign done = (state == 2)? 1:0;
-    assign arcsin = (done == 1)? reg_z[n]: 16'dz;
-    assign arccos = (done == 1)? ~{reg_z[n] - 16'b0110010010000111}+1:16'dz;
-    
-    
     wire [15:0] TANROM[15:0];
+    //a LUT used to store the arctan(2^-i), where i+1 is the current CORDIC iteration.
+    
+    //Setting values for the arctan LUT
     assign TANROM[0] = 16'b0011001001000011;
     assign TANROM[1] = 16'b0001110110101100;
     assign TANROM[2] = 16'b0000111110101101;
@@ -106,6 +94,7 @@ module arcsin_andarccos#(parameter n = 16) (input clk, input st, input signed [1
         endcase
     end
     
+    //performs the CORDIC iterations.
     always@(posedge clk)
     begin
         state<=nextstate;
@@ -133,5 +122,11 @@ module arcsin_andarccos#(parameter n = 16) (input clk, input st, input signed [1
             end
         end
     end
+    
+    assign result = (func == 2)? arccos :((func == 3)?arcsin:32'dz);
+    assign k = (i == n)? 1:0;
+    assign done = (state == 2)? 1:0;
+    assign arcsin = (done == 1)? reg_z[n]: 16'dz;
+    assign arccos = (done == 1)? ~{reg_z[n] - 16'b0110010010000111}+1:16'dz;
     
 endmodule
