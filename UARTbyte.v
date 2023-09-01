8d513fc6bf0791752265b859a3562be71005dd74
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-09-01 02:24:35 -0700

    added comments

--- UARTbyte.v
+++ UARTbyte.v
@@ -1,21 +1,43 @@
 `timescale 1ns / 1ps
+/*
+    File: UART.v
+    Author: Hamza Beder
+*/
 
+/*
+    This module sends a single byte to a user's computer using UART.
+    
+    Inputs:
+            clk: This is the clock signal used to control the operations of the module.
+            rst: Reset signal to bring signals to a known state.
+            data_send: The byte to be sent.
+            transmit: Start signal for sending data.
+            
+    Outputs:
+            Txd: The TX line used to send data to the user's laptop.
+            BRG_SET: BRG signal that is used in the Top module to control when bytes are sent.
+            Done: Set when the byte has been sent; this signal is used in the Top module to assist in sending multiple bytes.
+            
+           
+*/
 module UART_byte#(parameter cycle_BRG = 10416, idle = 2'b00, loading = 2'b01, sending = 2'b11, done = 2'b10, debounce_seconds = 2500)(
 input clk, input rst, input [7:0] data_send, input transmit, output reg Txd, output reg BRG_SET, output Done
     );
     
-    reg [13:0] BRG_counter;
-    reg [9:0] bit_counter;
+    reg [13:0] BRG_counter;// Counter used to implement the BRG.
+    reg [9:0] bit_counter;// Keeps track of the number of bits sent.
     
+    //SM control signals
     reg [1:0] state;
     reg [1:0] nextstate;
-    reg load = 0;
-    reg shift = 0;
-    reg clear = 0;
-    reg [9:0] shiftright_register;
-    reg[26:0] debounce_counter = 0;
+    reg load = 0; //Loads the shift register that stores the data along with the start and stop bits.
+    reg shift = 0; //Control signal for shifting the shiftirght_register bits onto the TX line.
+    reg clear = 0; //clears the bit counter when done sending.
+    reg [9:0] shiftright_register; //stores the bits to be sent.
     
-    assign Done = (state == done)? 1:0;
+    
+    reg[26:0] debounce_counter = 0; //Prevents issues with switch bounces when pressing the transmi button the Basys3.
+    
    //BRG, creating 9600 pulses per second
     always @(posedge clk)
     begin
@@ -35,7 +57,7 @@ input clk, input rst, input [7:0] data_send, input transmit, output reg Txd, out
     end
     
     //controller for UART
-    always @(transmit, state,BRG_SET, bit_counter)
+    always @(transmit, state, BRG_SET, bit_counter)
     begin
         nextstate<=idle;
         load<=0;
@@ -118,4 +140,5 @@ input clk, input rst, input [7:0] data_send, input transmit, output reg Txd, out
         end   
     end
     
+    assign Done = (state == done)? 1:0;
 endmodule
